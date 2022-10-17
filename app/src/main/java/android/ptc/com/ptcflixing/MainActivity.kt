90da9313d042ff5dc225aa58619c74a9fd7f85ac
karima <karimamohamed307@gmail.com>
2022-10-17 15:09:12 +0200

    -Fix gradle issues -Add android Jetpack dependencies

--- app/src/main/java/android/ptc/com/ptcflixing/MainActivity.kt
+++ app/src/main/java/android/ptc/com/ptcflixing/MainActivity.kt
@@ -1,12 +1,27 @@
 package android.ptc.com.ptcflixing
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.Text
+import androidx.activity.compose.setContent
+import androidx.compose.material3.CenterAlignedTopAppBar
 
-class MainActivity : AppCompatActivity() {
+import androidx.compose.runtime.Composable
+ import androidx.compose.ui.tooling.preview.Preview
+
+
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            launchAppView()
+        }
+    }
+    @Composable
+    @Preview
+    fun launchAppView(){
+
+Text(text = "SplachScreen")
     }
 }
